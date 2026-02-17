308bff2429fa72f2a39896f41a1d4b6d31739e1f
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2026-02-17 17:52:36 +0530

    update task page: adjust task title margin and add task count subtitle

--- main/static/tasks.js
+++ main/static/tasks.js
@@ -7,51 +7,56 @@ const taskInput = document.getElementById("taskInput");
 
 // Load tasks
 function loadTasks() {
-    const tasks = JSON.parse(localStorage.getItem("todos")) || [];
-    taskList.innerHTML = "";
+  const tasks = JSON.parse(localStorage.getItem("todos")) || [];
+  taskList.innerHTML = "";
 
-    tasks.forEach((task, index) => {
-        const li = document.createElement("li");
-        li.className = "task-item";
-        li.textContent = task;
+  if (tasks.length >= 0) {
+    const taskSubTitle = document.getElementById("task-container-sub-header");
+    taskSubTitle.textContent = `You have ${tasks.length} tasks, Click to delete them.`;
+  }
 
-        // Delete on click
-        li.addEventListener("click", () => {
-            tasks.splice(index, 1);
-            localStorage.setItem("todos", JSON.stringify(tasks));
-            loadTasks();
-        });
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.className = "task-item";
+    li.textContent = task;
 
-        taskList.appendChild(li);
+    // Delete on click
+    li.addEventListener("click", () => {
+      tasks.splice(index, 1);
+      localStorage.setItem("todos", JSON.stringify(tasks));
+      loadTasks();
     });
+
+    taskList.appendChild(li);
+  });
 }
 
 // Open modal
 addBtn.onclick = () => {
-    modal.classList.remove("hidden");
-    taskInput.focus();
+  modal.classList.remove("hidden");
+  taskInput.focus();
 };
 
 // Close modal
 cancelBtn.onclick = () => {
-    modal.classList.add("hidden");
-    taskInput.value = "";
+  modal.classList.add("hidden");
+  taskInput.value = "";
 };
 
 // Save task
 saveBtn.onclick = () => {
-    const task = taskInput.value.trim();
-    if (!task) return;
+  const task = taskInput.value.trim();
+  if (!task) return;
 
-    const tasks = JSON.parse(localStorage.getItem("todos")) || [];
-    tasks.push(task);
+  const tasks = JSON.parse(localStorage.getItem("todos")) || [];
+  tasks.push(task);
 
-    localStorage.setItem("todos", JSON.stringify(tasks));
+  localStorage.setItem("todos", JSON.stringify(tasks));
 
-    taskInput.value = "";
-    modal.classList.add("hidden");
+  taskInput.value = "";
+  modal.classList.add("hidden");
 
-    loadTasks();
+  loadTasks();
 };
 
 // Load on start
