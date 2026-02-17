f03c864c9be702571021e69cb9e0917a69f3a783
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2026-02-17 17:00:12 +0530

    added basic home page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Main App Working ✅")
+    return render(request, "home.html")
