36a21e93d2cde4448fc7a02c5beff11c85fa19db
devShadow <147122977+kaushal-Prakash@users.noreply.github.com>
2026-02-24 14:56:27 +0530

    offset and time

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('tasks/', views.tasks, name='tasks'),
+    path('time/<int:offset>/', views.time, name='time'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
+import datetime
 
 def home(request):
     return render(request, "home.html")
 
 def tasks(request):
     return render(request, "tasks.html")
+
+def time(request, offset):
+    offset = int(offset)
+
+    curr_time = datetime.datetime.now()
+
+    past = curr_time - datetime.timedelta(hours=offset)
+    future = curr_time + datetime.timedelta(hours=offset)
+
+    return render(
+        request,
+        "time.html",
+        context={
+            "past_time": past,
+            "future_time": future,
+            "current_time": curr_time,
+            "offset": offset,
+        },
+    )
